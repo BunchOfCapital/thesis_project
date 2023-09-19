82fd482abe03d0c6c46d837266561db99e828e9b
Joel Wildman <joel.l.wildman@gmail.com>
2023-09-19 09:51:32 +0800

    more simulations for heatmaps

--- heatmap.py
+++ heatmap.py
@@ -10,5 +10,6 @@ for i in range(len(contents)):
 	for j in range(len(contents[i])):
 		contents[i][j] = float(contents[i][j])
 print(contents)
-plt.imshow(contents, cmap='Greys', interpolation='nearest', vmin=0, vmax=1)
+#contents.reverse()
+plt.imshow(contents, cmap='Greys', interpolation='nearest', vmin=0.4, vmax=0.7, origin="lower")
 plt.show()
