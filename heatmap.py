592a0a2addcae939fb44205fd818d744610bfc4e
Joel Wildman <joel.l.wildman@gmail.com>
2023-10-05 18:38:51 +0800

    added ppt pres

--- heatmap.py
+++ heatmap.py
@@ -4,22 +4,26 @@ import sys
 
 filename = sys.argv[1]
 file = open(filename, "r")
+final_map = np.zeros((11, 11))
+for data in range(10):
+	fname = "int_op_og_heatmap_" + str(data)
+	file = open(fname, "r")
+	contents = file.readlines()
+	for i in range(len(contents)):
+		contents[i] = contents[i].split(",")
 
-contents = file.readlines()
-for i in range(len(contents)):
-	contents[i] = contents[i].split(",")
-
-for i in range(len(contents)):
-	for j in range(len(contents[i])):
-		contents[i][j] = float(contents[i][j])
-print(contents)
+	for i in range(len(contents)):
+		for j in range(len(contents[i])):
+			contents[i][j] = float(contents[i][j])
+	print(contents)
+	final_map += np.array(contents)
 #contents.reverse()
-
+final_map = final_map/10
 fig, ax = plt.subplots()
 
-heatmap = plt.pcolor(contents, cmap='Greys')
+heatmap = plt.pcolor(final_map, cmap='Greys')
 plt.colorbar(heatmap)
-plt.title("Average Internal Opinion after 30 Iterations (Facebook Network) ")
+plt.title("Average Internal Opinion after 30 Iterations (Lattice Network) ")
 ax.set_yticks(np.round(np.linspace(0, 10, num=11), 1), labels=np.round(np.linspace(0.0, 1.0, num=11), 1))
 ax.set_xticks(np.round(np.linspace(0, 10, num=11), 1), labels=np.round(np.linspace(0.0, 1.0, num=11), 1))
 plt.xlabel("P")
